a42b3e31704281465dc2f7343bc094800fecfd72
Kusyun <kusyunexcl@gmail.com>
2019-10-25 00:31:47 +0800

    加入專案檔案。

---
+++ LZ78/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LZ78 {
+    partial class Form1 {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(560, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(375, 89);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Read File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(560, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(375, 89);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Encode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(560, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(375, 89);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Decode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(542, 394);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 417);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox2.Size = new System.Drawing.Size(542, 394);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.WordWrap = false;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(560, 297);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox3.Size = new System.Drawing.Size(375, 514);
+            this.richTextBox3.TabIndex = 7;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 823);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lz 78 編碼";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ LZ78/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace LZ78 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e) {
+            string filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                openFileDialog.Filter = "All files (*.*)|*.*";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    filePath = openFileDialog.FileName;
+                }
+            }
+
+            if (filePath.Length == 0) return;
+
+            string filecontext = string.Empty;
+            byte[] tempContext = System.IO.File.ReadAllBytes(filePath);
+            filecontext = BitConverter.ToString(tempContext);
+            filecontext = filecontext.Replace("-", "");
+            richTextBox1.Text = filecontext;
+        }
+
+        private void Button2_Click(object sender, EventArgs e) {
+            string data = richTextBox1.Text;
+            string tempStr = "";
+            string encodeStr = "";
+            int lastID = 0;
+            int ID = 0;
+
+            List<string> Dictionary = new List<string>();
+            Dictionary.Add("");
+
+            int nowid = 0;
+            foreach (char str in data) {
+                nowid += 1;
+
+                tempStr += str;
+                ID = Dictionary.FindIndex(tmp => tmp.Equals(tempStr));
+
+                if (ID == -1) {
+                    Dictionary.Add(tempStr);
+                    //encodeStr += "(" + lastID + "," + tempStr[tempStr.Length - 1] + ")";
+                    encodeStr += lastID + "," + tempStr[tempStr.Length - 1] + "|";
+                    tempStr = "";
+                    lastID = 0;
+                } else {
+
+                    if (nowid == data.Length ) {
+                        encodeStr += ID + ",|";
+                    }
+                    lastID = ID;
+                }
+            }
+
+            richTextBox2.Text = encodeStr;
+            encodeStr = "";
+            foreach (string str in Dictionary) {
+                encodeStr += str + "\n";
+            }
+            richTextBox3.Text = encodeStr;
+        }
+
+        private void Button3_Click(object sender, EventArgs e) {
+            List<string> Dictionary = new List<string>();
+            Dictionary.AddRange(richTextBox3.Text.Split('\n'));
+            string decodeStr = "";
+            string tempStr;
+            int tempID;
+
+            foreach(string str in richTextBox2.Text.Split('|')) {
+                if (str.Contains(',')) {
+                    tempID = Convert.ToInt32(str.Split(',')[0]);
+                    tempStr = str.Split(',')[1];
+
+                    if (tempID == 0) {
+                        decodeStr += tempStr;
+                    } else {
+                        decodeStr += Dictionary[tempID] + tempStr;
+                    }
+                    
+                }
+            }
+
+            richTextBox1.Text = decodeStr;
+        }
+    }
+}
